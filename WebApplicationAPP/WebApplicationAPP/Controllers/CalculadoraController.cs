95d13be30f632301a0950755bd33f837d9404de3
marioaje <marioaje@gmail.com>
2026-01-23 20:13:50 -0600

    Proyecto Web

---
+++ WebApplicationAPP/WebApplicationAPP/Controllers/CalculadoraController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationAPP.Models;
+
+namespace WebApplicationAPP.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculadoraViewModel modelo)
+        {
+
+            return View(modelo);
+        }
+    }
+}
---
+++ WebApplicationAPP/WebApplicationAPP/Models/CalculadoraViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplicationAPP.Models
+{
+    public class CalculadoraViewModel
+    {
+        public double valor1 { get; set; }
+
+        public double valor2 { get; set; }
+
+        public double resultado { get; set; }
+
+        public string operacion { get; set; }
+        //?????
+    }
+}
