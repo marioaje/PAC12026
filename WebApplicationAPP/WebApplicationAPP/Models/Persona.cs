0c3ad46376489c57a322df6254f8e9694c18f84f
marioaje <marioaje@gmail.com>
2026-02-06 18:39:39 -0600

    mysql

---
+++ WebApplicationAPP/WebApplicationAPP/Models/Persona.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace WebApplicationAPP.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public bool Estado { get; set; }
+
+
+        //      CREATE TABLE `u484426513_pac126`.`Persona` (
+        //`Id` INT NULL AUTO_INCREMENT,
+        //`Nombre` VARCHAR(100) NOT NULL,
+        //`Apellido` VARCHAR(100) NOT NULL,
+        //`Estado` TINYINT(1) NOT NULL,
+        //PRIMARY KEY(`Id`));
+
+
+    }
+}
