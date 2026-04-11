4480506bc66002c755fe0aff3951cb579bb450c6
marioaje <marioaje@gmail.com>
2026-04-10 18:41:12 -0600

    API

---
+++ WebApplicationAPP/API/API/Controllers/ProductosController.cs
@@ -0,0 +1,91 @@
+﻿using API.Model;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Timers;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : Controller
+    {
+        private readonly ProductoSerice _productoSerice;
+
+
+        public ProductosController(ProductoSerice productoSerice)
+        {
+            _productoSerice = productoSerice;
+        }
+        //El controller une la vista con la logica 
+        //CRUD POST/GET/PUT/DELETE
+
+        //GetProducto()
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductoModel>> GetProducto()
+        {
+            return _productoSerice.GetProducto();
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult <ProductoModel> GetByIdProducto(int id)
+        {
+            return _productoSerice.GetByIdProducto(id);
+        }
+
+        [HttpPost]
+        public ActionResult PostProducto(ProductoModel _productoModel) {
+
+            ProductoModel newProducto = _productoSerice.PostProducto(_productoModel);
+
+
+            return CreatedAtAction(
+                    nameof(GetProducto),
+                    new
+                        {
+                            id = newProducto.Precio
+                        },
+                        newProducto );
+
+        }
+
+        [HttpPut]
+        public ActionResult PutProducto(ProductoModel _productoModel)
+        {
+            if ( !_productoSerice.PutProducto(_productoModel))
+            {
+                return NotFound(
+                     new
+                     {
+                         Mensaje ="No se encontro el registro"
+                     }
+                    );
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpDelete]
+        public ActionResult DeleteProducto(int _id)
+        {
+            if (!_productoSerice.DeleteProducto(_id))
+            {
+                return NotFound(
+                     new
+                     {
+                         Mensaje = "No se encontro el registro"
+                     }
+                    );
+            }
+
+            return NoContent();
+
+        }
+
+
+    }
+}
---
+++ WebApplicationAPP/API/API/Services/ProductoSerice.cs
@@ -0,0 +1,91 @@
+﻿using API.Data;
+using API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Services
+{
+    public class ProductoSerice
+    {
+        private readonly AppDbContext _context;
+        
+
+        //El iniciador???
+        public ProductoSerice(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //Funcion de obtener datos
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Producto.ToListAsync());
+        //}
+        //GET
+        public List<ProductoModel> GetProducto()
+        {
+            return _context.Producto.ToList();
+        }
+
+
+        //GET/ID
+        public ProductoModel GetByIdProducto(int _id)
+        {
+            return _context.Producto.FirstOrDefault(p => p.Id == _id);
+        }
+
+
+        //POST
+        public ProductoModel PostProducto(ProductoModel _productoModel)
+        {
+            //                 _context.Add(_producto);
+            //await _context.SaveChangesAsync();
+            _context.Producto.Add(_productoModel);
+            _context.SaveChanges();
+
+            return _productoModel;
+
+        }
+
+        //PUT
+        public bool PutProducto(ProductoModel _productoModel)
+        {
+
+            var entidad = _context.Producto.FirstOrDefault(p => p.Id == _productoModel.Id);
+
+            if (entidad == null)
+            {
+                return false;
+            }
+
+
+            entidad.Nombre = _productoModel.Nombre;
+            entidad.Precio = _productoModel.Precio;
+            entidad.Cantidad = _productoModel.Cantidad;
+            entidad.Descripcion = _productoModel.Descripcion;
+
+            _context.SaveChanges();
+
+            return true;
+        }
+
+
+        //DELETE
+        public bool DeleteProducto(int _id)
+        {
+
+            var entidad = _context.Producto.FirstOrDefault(p => p.Id == _id);
+
+            if (entidad == null)
+            {
+                return false;
+            }
+
+            _context.Producto.Remove(entidad);
+            _context.SaveChanges();
+
+            return true;
+        }
+    }
+}
